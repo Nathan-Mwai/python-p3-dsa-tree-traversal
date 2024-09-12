3076be58aa82eadb1346a1d9ca773f5372f0c43a
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-09-12 06:37:03 +0300

    finish labs

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,13 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
+    output = []
+    output.append(self.root)
+    while output:
+      node = output.pop()
+      if node['id'] == id:
+        return node
+      else:
+        for child in node['children']:
+          output.append(child)
     pass
